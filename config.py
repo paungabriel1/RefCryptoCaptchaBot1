5bbbf506fb9fa3b336c3c3cb9b295f075921f95e
paungabriel1 <91350962+paungabriel1@users.noreply.github.com>
2024-03-09 23:10:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,49 +1,38 @@
 ### ---___--- ТОКЕН ОТ ТЕЛЕГРАМ БОТА ---___--- ###
-bot_api = "TOKEN"
-
+bot_api = "5927265893:AAFU7-TAs1T5logNDewHf6xB9NDQo0Q5Ywo"
 
 
 ### ---___--- НАЗВАНИЕ КАНАЛА ---___--- ###
 channels = {
-    "https://t.me/+YUsmkD8txfw3Nzli": -1001648480843,
-    "@walldevnew": -1001325790527,
-    }
-
-
+        "<a href='https://t.me/+pcwOj0R9gKhmOTMy'>Crypto | Notcoin | Binance</a>": -1001883735444,
+}
 
 ### ---___--- ТЕХНЧЕСКАЯ ПОДДЕРЖКА ---___--- ###
-technical_support  = ""
-
+technical_support  = "@tiger_opasniy"
 
 
 ### ---___--- КАНАЛ ДЛЯ ОТЗЫВОВ ---___--- ###
-channel_with_feedback  = False #Канал с отзывами, если он не нужен, то уберите кавычки напишите False
-
+channel_with_feedback  = False #Канал с отзывами, если он не нужен, то уберите кавычки и напишите False
 
 
 ### ---___--- ID АДМИНОВ ИЗ ТЕЛЕГРАМА ---___--- ###
-id_admins = [1594375693]
-
+id_admins = [238571313]
 
 
 ### ---___--- СУММА ЗА ОДНОГО РЕФЕРАЛА ---___--- ###
-amount_per_one = 5
-
+amount_per_one = 0.5
 
 
 ### ---___--- МИНИМАЛЬНАЯ СУММА ВЫВОДА ---___--- ###
-minimal_withdrawal_amount = 50
-
+minimal_withdrawal_amount = 3
 
 
 ### ---___--- ОТОБРАЖАЕМАЯ ВАЛЮТА ---___--- ###
-currency = "RUB" # USD или RUB
-
+currency = "USD" # USD или RUB
 
 
 ### ---___--- ОТОБРАЖАЕМАЯ КРИПТОВАЛЮТА ---___--- ###
-cryptocurrency = "TRX" # BTC или ETH или USDT или BNB или SOL или XRP или ADA или DOGE или TON
-
+cryptocurrency = "TON" # BTC или ETH или USDT или BNB или SOL или XRP или ADA или DOGE или TON
 
 
 ### ---___--- ЯЗЫК БОТА ---___--- ###
